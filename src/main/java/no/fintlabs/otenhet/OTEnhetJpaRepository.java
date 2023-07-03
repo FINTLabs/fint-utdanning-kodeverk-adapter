61134cf6f586ed7751ac0997d7d4e281896b12b0
Hknots <Hknots99@gmail.com>
2023-07-03 10:39:17 +0200

    add repository annotation

--- src/main/java/no/fintlabs/otenhet/OTEnhetJpaRepository.java
+++ src/main/java/no/fintlabs/otenhet/OTEnhetJpaRepository.java
@@ -1,6 +1,8 @@
 package no.fintlabs.otenhet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OTEnhetJpaRepository extends JpaRepository<OTEnhetEntity, String> {
 }
